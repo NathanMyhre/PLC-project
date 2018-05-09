341556ddf34fdbdf384d7bfd9a0200adaabea15a
Nate Myhre <nmyhre@diane-latinos-macbook-pro.local>
2018-05-09 15:05:44 -0500

    Got stuck on mums, so tried to continue figuring out STree. Got stuck again. So close

--- mums.hs
+++ mums.hs
@@ -14,7 +14,7 @@ dfsPractice (Node ((str, tree) : next)) =
   let x = (map getTree next) in
     (dfsPractice tree) ++ (foldr (++) [] (map dfsPractice x))
 
--- finds the number of internal nodes in a Suffix Tree.
+-- Finds the number of internal nodes in a Suffix Tree.
 numberOfNodes :: Int -> SuffixTree  -> Int
 numberOfNodes n (Leaf n') = 0
 numberOfNodes n (Node []) = 0
@@ -22,6 +22,25 @@ numberOfNodes n (Node ( (str, tree) : next) ) =
   let x = (map getTree next) in
     1 + (numberOfNodes n tree) + (foldr (+) 0 (map (numberOfNodes n ) x)) 
 
+-- Finds if a substring exists within a suffix tree and return it.
+-- Returns a list of substrings, along with number of nodes it traversed.
+findPath :: String -> SuffixTree -> Maybe [(String, Int)]
+findPath (s : sx) Leaf n' = Nothing
+findPath (s : sx) (Node ( (str, tree) : next) ) = []
+  
+
+-- Find common prefix between two strings
+commonPrefix :: Eq a => [a] -> [a] -> [a]
+commonPrefix [] _ = []
+commonPrefix _ [] = []
+commonPrefix (x : xs) (y : ys) = if x == y then (x : (commonPrefix xs ys))
+                             else []
+-- Return the uncommon suffixes 
+uncommonSuffix :: Eq a => [a] -> [a] -> ([a], [a])
+uncommonSuffix [] b = ([], b)
+uncommonSuffix a [] = (a, [])
+uncommonSuffix (x : xs) (y : ys) = if x == y then (uncommonSuffix xs ys)
+                                   else ( (x : xs), (y : ys) )
 
 {-Finding Maximum Unique matching substrings.
   - Take two strings and build a Suffix Tree out of their concatenation
--- suffix-tree.hs
+++ suffix-tree.hs
@@ -6,8 +6,6 @@ type Edge = (String, STree)
 data STree = Leaf Int | Node [Edge]
   deriving Show
 
-
-
 -- Adds the terminal character, "$" to a string
 -- Need to add the terminal character somewhere else. Maybe within mkTree?
 addTerminal ::  String -> String
@@ -23,8 +21,9 @@ insertEdge (s, n) (Node xs) = Node (xs ++ (s , Leaf n) : [])
 -}
 splitEdge :: (String, Int) -> Edge -> Edge
 splitEdge ([], n) anything = ([], Leaf n)
-splitEdge (str, n) (str', subtree) = let (x, y) = (uncommonSuffix str str') in
-                      ((commonPrefix str str'), Node [(y, subtree), (x, Leaf n)])
+splitEdge (str, n) (str', subtree) =
+  let (x, y) = (uncommonSuffix str str') in
+    ((commonPrefix str str'), Node [(x, Leaf n), (y, subtree)])
 
 -- helpers for splitEdge -------------------------------------------------------
 -- Find common prefix between two strings
@@ -39,21 +38,31 @@ uncommonSuffix [] b = ([], b)
 uncommonSuffix a [] = (a, [])
 uncommonSuffix (x : xs) (y : ys) = if x == y then (uncommonSuffix xs ys)
                                    else ( (x : xs), (y : ys) )
---------------------------------------------------------------------------------
-
--- Searches edges leaving a single node for a desired prefix.
--- Just a dumb search algorithm, we're only using it with 4 letters so
--- no need for efficiency.
-{-findPrefix :: String -> STree -> Bool
-findPrefix s Leaf = False
-findPrefix s (Node []) = False
-findPrefix s (Node ((prefix, tree) : edges) ) =
-           if (head s) == (head prefix) then True
-           else (findPrefix s (Node edges) ) -}
 
 -- Finds a path within tree, given a String
 -- note: Make sure insertEdge retains the old node too.
+
 findAndInsert :: (String, Int) -> STree -> STree
+findAndInsert (s, n) node = case node of
+  Node [] -> insertEdge (s, n) node
+  Node ( (str, tree) : next) ->
+    let listOfEdges = (map (findAndInsertE (s, n)) next) in
+      if commonPrefix s str /= [] then
+            Node ( (findAndInsertE (s, n) (str, tree)) : next)
+      else Node ((str, tree) : listOfEdges)
+           
+findAndInsertE :: (String, Int) -> Edge -> Edge
+findAndInsertE (s, n) (str, tree) = let commStr = commonPrefix s str in
+  case tree of
+    Leaf n -> if commStr /= [] then splitEdge (s, n) (str, tree)
+              else (str, tree)
+    Node t -> let (x, y) = uncommonSuffix s str in
+              if commStr /= [] then
+                if y /= [] then splitEdge (s, n) (str, tree)--prefix not eaten
+                else (commStr, (findAndInsert (s, n) tree))
+              else (str, (findAndInsert (s, n) tree))
+
+  {-findAndInsert :: (String, Int) -> STree -> STree
 findAndInsert (s, n) node = case node of
      Node [] -> insertEdge (s, n) node
      Node ((first, tree) : next) -> if commonPrefix s first == [] then
@@ -63,26 +72,15 @@ findAndInsert (s, n) node = case node of
                 Leaf n' -> Node ((splitEdge (s, n) (first,tree)) : next)
                 Node edges -> Node ((first, (findAndInsert (s,n) tree)) : next)
       
-       
+   -}    
 mkTree :: String -> STree
 mkTree [] = Node []
 
-
--- Helper for Show functions ---------------------------------------------------
-
-{--- Convert edges to String
--- Convert nodes to String
-
-showSTree :: Show (STree) => STree -> String
-showSTree tree = ""
-
-instance Show (STree) where
-  show t = showSTree t
--}
+-- Test variables --------------------------------------------------------------
 
 
 testTree :: STree
-testTree =Node [("ab",Node [("",Leaf 3),("cab",Leaf 0)]),("b",Node [("",Leaf 4),("cab",Leaf 1)]),("cab",Leaf 2)]
+testTree = Node [("ab",Node [("",Leaf 3),("cab",Leaf 0)]),("b",Node [("",Leaf 4),("cab",Leaf 1)]),("cab",Leaf 2)]
 
 testTreeInc = Node [("abcab", Leaf 0)]
 
@@ -91,9 +89,9 @@ testTree1 = Node []
 
 testString = "abcab$"
 
-{-a = findAndInsert "abcab$" testTree1
-b = findAndInsert "bcab$" (Node [("abcab$", Leaf)])
-c = findAndInsert "cab$" b
-d = findAndInsert "ab$" c
-e = findAndInsert "b$" d
-f = findAndInsert "$" e-}
+a = findAndInsert ("abcab$", 0) testTree1
+b = findAndInsert ("bcab$", 1) (Node [("abcab$", Leaf 0)])
+c = findAndInsert ("cab$", 2) b
+d = findAndInsert ("ab$", 3) c
+e = findAndInsert ("b$", 4) d
+f = findAndInsert ("$", 5) e
